340b36c9893844633de47eaec787cf167a4707ea
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-13 08:09:22 +0300

    Done with this assignment

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 // Code your solution here
-
-// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-//  const findMatching=drivers.filter(function (names) {
-//     return['Bobby', 'Sammy', 'Bobby'] 
-// })
-// console.log(findMatching)
-
-
-
 function findMatching(drivers, name){
     return drivers.filter(function (d) {return (d.toUpperCase() == name.toUpperCase())});
 }
