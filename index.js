508831690da70eefbde9823f48c159d893f9fe3e
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-13 08:07:52 +0300

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//  const findMatching=drivers.filter(function (names) {
+//     return['Bobby', 'Sammy', 'Bobby'] 
+// })
+// console.log(findMatching)
+
+
+
+function findMatching(drivers, name){
+    return drivers.filter(function (d) {return (d.toUpperCase() == name.toUpperCase())});
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(function (d) {return (d.charAt(0) === name.charAt(0))});
+}
+
+function matchName(driver, str){
+    return driver.filter(function (d) {return (d.name === str)});
+}
+    
